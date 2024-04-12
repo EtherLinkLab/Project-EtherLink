8e0c2b4786090d5c12460a3771855caa14e72bca
EtherStellar <166634373+EtherStellarLab@users.noreply.github.com>
2024-04-12 16:49:54 +0600

    Update EtherStellar (#dev).sol

--- EtherStellar (#dev).sol
+++ EtherStellar (#dev).sol
@@ -1054,7 +1054,7 @@ abstract contract Ownable is Context {
 }
 
 
-
+// File: @openzeppelin/contracts/token/ERC20/IERC20.sol
 // OpenZeppelin Contracts (last updated v4.6.0) (token/ERC20/IERC20.sol)
 
 pragma solidity ^0.8.22;
@@ -1135,4 +1135,36 @@ interface IERC20 {
         address to,
         uint256 amount
     ) external returns (bool);
+}
+
+
+
+// File: @openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol
+
+
+// OpenZeppelin Contracts v4.4.1 (token/ERC20/extensions/IERC20Metadata.sol)
+
+pragma solidity ^0.8.22;
+
+
+/**
+ * @dev Interface for the optional metadata functions from the ERC20 standard.
+ *
+ * _Available since v4.1._
+ */
+interface IERC20Metadata is IERC20 {
+    /**
+     * @dev Returns the name of the token.
+     */
+    function name() external view returns (string memory);
+
+    /**
+     * @dev Returns the symbol of the token.
+     */
+    function symbol() external view returns (string memory);
+
+    /**
+     * @dev Returns the decimals places of the token.
+     */
+    function decimals() external view returns (uint8);
 }
