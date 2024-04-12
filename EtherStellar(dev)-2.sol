0758f5977bad5f0166e836ef184e792a34c066c8
EtherStellar <166634373+EtherStellarLab@users.noreply.github.com>
2024-04-12 16:19:15 +0600

    Update EtherStellar(dev)-2.sol

--- EtherStellar(dev)-2.sol
+++ EtherStellar(dev)-2.sol
@@ -300,7 +300,7 @@ pragma solidity ^0.8.22;
  * ```solidity
  * contract ERC1967 {
  *     // Define the slot. Alternatively, use the SlotDerivation library to derive the slot.
- *     bytes32 internal constant _IMPLEMENTATION_SLOT = 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
+ *     bytes32 internal constant _IMPLEMENTATION_SLOT = 0x252fd9C54323240Cc1129beD11a1fE891fEb9be6;
  *
  *     function _getImplementation() internal view returns (address) {
  *         return StorageSlot.getAddressSlot(_IMPLEMENTATION_SLOT).value;
@@ -319,7 +319,7 @@ pragma solidity ^0.8.22;
  * ```solidity
  * contract Lock {
  *     // Define the slot. Alternatively, use the SlotDerivation library to derive the slot.
- *     bytes32 internal constant _LOCK_SLOT = 0xf4678858b2b588224636b8522b729e7722d32fc491da849ed75b3fdf3c84f542;
+ *     bytes32 internal constant _LOCK_SLOT = 0xB8258711507Deb527F8E46c09EC443F9E162D906;
  *
  *     modifier locked() {
  *         require(!_LOCK_SLOT.asBoolean().tload());
@@ -620,7 +620,7 @@ library StorageSlot {
 // OpenZeppelin Contracts (last updated v5.0.0) (proxy/ERC1967/ERC1967Proxy.sol)
 
 
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.22;
 
 
 /**
@@ -658,7 +658,62 @@ contract ERC1967Proxy is Proxy {
 
 
 
-// File: @Breadcrumbsopenzeppelin-contracts/contracts/proxy/beacon/BeaconProxy.sol
+// File: @openzeppelin-contracts/contracts/proxy/beacon/BeaconProxy.sol
+// SPDX-License-Identifier: MIT
+
+// OpenZeppelin Contracts (last updated v5.0.0) (proxy/beacon/BeaconProxy.sol)
+
+
+pragma solidity ^0.8.22;
+
+/**
+ * @dev This contract implements a proxy that gets the implementation address for each call from an {UpgradeableBeacon}.
+ *
+ * The beacon address can only be set once during construction, and cannot be changed afterwards. It is stored in an
+ * immutable variable to avoid unnecessary storage reads, and also in the beacon storage slot specified by
+ * https://eips.ethereum.org/EIPS/eip-1967[ERC-1967] so that it can be accessed externally.
+ *
+ * CAUTION: Since the beacon address can never be changed, you must ensure that you either control the beacon, or trust
+ * the beacon to not upgrade the implementation maliciously.
+ *
+ * IMPORTANT: Do not use the implementation logic to modify the beacon storage slot. Doing so would leave the proxy in
+ * an inconsistent state where the beacon storage slot does not match the beacon address.
+ */
+contract BeaconProxy is Proxy {
+    // An immutable address for the beacon to avoid unnecessary SLOADs before each delegate call.
+    address private immutable _beacon;
+
+    /**
+     * @dev Initializes the proxy with `beacon`.
+     *
+     * If `data` is nonempty, it's used as data in a delegate call to the implementation returned by the beacon. This
+     * will typically be an encoded function call, and allows initializing the storage of the proxy like a Solidity
+     * constructor.
+     *
+     * Requirements:
+     *
+     * - `beacon` must be a contract with the interface {IBeacon}.
+     * - If `data` is empty, `msg.value` must be zero.
+     */
+    constructor(address beacon, bytes memory data) payable {
+        ERC1967Utils.upgradeBeaconToAndCall(beacon, data);
+        _beacon = beacon;
+    }
+
+    /**
+     * @dev Returns the current implementation address of the associated beacon.
+     */
+    function _implementation() internal view virtual override returns (address) {
+        return IBeacon(_getBeacon()).implementation();
+    }
+
+    /**
+     * @dev Returns the beacon.
+     */
+    function _getBeacon() internal view virtual returns (address) {
+        return _beacon;
+    }
+}
 
 
 
