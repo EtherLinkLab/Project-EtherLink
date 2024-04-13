ac3ef6780b35e23a4a2d964b1aacffecc2405d95
EtherStellar <166634373+EtherStellarLab@users.noreply.github.com>
2024-04-13 23:42:56 +0600

    Update EtherStellar Coin (dev).sol

--- EtherStellar Coin (dev).sol
+++ EtherStellar Coin (dev).sol
@@ -1068,13 +1068,14 @@ abstract contract Ownable is Context {
 }
 
 
+
 // File: @openzeppelin-contracts/contracts/access/IAccessControl.sol
 // SPDX-License-Identifier: MIT
 
 // OpenZeppelin Contracts (last updated v5.0.0) (access/IAccessControl.sol)
 
 
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.22;
 
 
 /**
@@ -1173,12 +1174,76 @@ interface IAccessControl {
 
 
 
+// File: @openzeppelin-contracts/contracts/utils/introspection/IERC165.sol
+// SPDX-License-Identifier: MIT
+
+// OpenZeppelin Contracts (last updated v5.0.0) (utils/introspection/IERC165.sol)
+
+
+pragma solidity ^0.8.22;
+
+
+/**
+ * @dev Interface of the ERC-165 standard, as defined in the
+ * https://eips.ethereum.org/EIPS/eip-165[ERC].
+ *
+ * Implementers can declare support of contract interfaces, which can then be
+ * queried by others ({ERC165Checker}).
+ *
+ * For an implementation, see {ERC165}.
+ */
+interface IERC165 {
+    /**
+     * @dev Returns true if this contract implements the interface defined by
+     * `interfaceId`. See the corresponding
+     * https://eips.ethereum.org/EIPS/eip-165#how-interfaces-are-identified[ERC section]
+     * to learn more about how these ids are created.
+     *
+     * This function call must use less than 30 000 gas.
+     */
+    function supportsInterface(bytes4 interfaceId) external view returns (bool);
+}
+
+
+// File: @openzeppelin-contracts/contracts/utils/introspection/ERC165.sol
+// SPDX-License-Identifier: MIT
+
+// OpenZeppelin Contracts (last updated v5.0.0) (utils/introspection/ERC165.sol)
+
+
+pragma solidity ^0.8.22;
+
+
+/**
+ * @dev Implementation of the {IERC165} interface.
+ *
+ * Contracts that want to implement ERC-165 should inherit from this contract and override {supportsInterface} to check
+ * for the additional interface id that will be supported. For example:
+ *
+ * ```solidity
+ * function supportsInterface(bytes4 interfaceId) public view virtual override returns (bool) {
+ *     return interfaceId == type(MyInterface).interfaceId || super.supportsInterface(interfaceId);
+ * }
+ * ```
+ */
+abstract contract ERC165 is IERC165 {
+    /**
+     * @dev See {IERC165-supportsInterface}.
+     */
+    function supportsInterface(bytes4 interfaceId) public view virtual returns (bool) {
+        return interfaceId == type(IERC165).interfaceId;
+    }
+}
+
+
+
 // File: @openzeppelin-contracts/contracts/access/AccessControl.sol
 // SPDX-License-Identifier: MIT
+
 // OpenZeppelin Contracts (last updated v5.0.0) (access/AccessControl.sol)
 
 
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.22;
 
 
 /**
@@ -1992,7 +2057,7 @@ abstract contract Pausable is Context {
 // OpenZeppelin Contracts (last updated v5.0.0) (utils/ReentrancyGuard.sol)
 
 
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.22;
 
 
 /**
